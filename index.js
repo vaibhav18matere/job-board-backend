035c9cb3ee159e2fea5f4e1642f287d796598de6
VaibhavMatere <vpm00718897@gmail.com>
2024-09-25 17:29:17 +0530

    initialise project on localhost 3000

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from 'cors';
+
+
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+     origin: "https//localhost:5173",
+     credentials: true,
+};
+
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+     console.log("server running at port", PORT);
+});
