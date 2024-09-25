4683a3569b3b100145620a50aaa54e5e35b76a90
VaibhavMatere <vpm00718897@gmail.com>
2024-09-25 23:24:31 +0530

    logged out user controller logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -121,3 +121,16 @@ export const loginUser = async (req, res) => {
           console.log(error);
      };
 };
+
+// logout user
+
+export const logoutUser = async (req, res) => {
+     try {
+          return res.status(200).cookie("token", "", { maxAge: 0 }).json({
+               message: "User logged out successfully!",
+               success: true
+          });
+     } catch (error) {
+          console.log(error)
+     };
+};
