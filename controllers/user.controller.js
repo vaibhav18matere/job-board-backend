6b59c89512ccc7896a77e5309edb410a2dac6f7f
VaibhavMatere <vpm00718897@gmail.com>
2024-09-26 16:56:20 +0530

    update user details controller logic

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -134,3 +134,55 @@ export const logoutUser = async (req, res) => {
           console.log(error)
      };
 };
+
+// update user details
+
+
+export const updateUserProfile = async (req, res) => {
+     try {
+          const { fullname, email, phoneNumber, bio, skills } = req.body;
+
+          let skillsArray;
+          if (skills) {
+               skillsArray = skills.split(",");
+          };
+
+          const userId = req.id; // middleware authentication
+          let user = await User.findById(userId);
+
+          if (!user) {
+               return res.status(400).json({
+                    message: "User not found!",
+                    success: false
+               });
+          };
+
+          // updating data
+
+          if (fullname) user.fullname = fullname
+          if (email) user.email = email
+          if (phoneNumber) user.phoneNumber = phoneNumber
+          if (bio) user.profile.bio = bio
+          if (skills) user.profile.skills = skillsArray
+
+          await user.save();
+
+          user = {
+               _id: user._id,
+               fullname: user.fullname,
+               email: user.email,
+               phoneNumber: user.phoneNumber,
+               role: user.role,
+               profile: user.profile
+          };
+
+          return res.status(200).json({
+               message: "User profile updated successfully!",
+               user,
+               success: true
+          });
+
+     } catch (error) {
+          console.log(error);
+     };
+};
