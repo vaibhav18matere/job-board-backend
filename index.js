15eb1d6b2b04897eea4c33e40908c79c1a162481
VaibhavMatere <vpm00718897@gmail.com>
2024-09-26 16:57:51 +0530

    user routes and relevant functionality completed

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import cors from 'cors';
 import dotenv from 'dotenv'
 import { connectDB } from "./utils/db.js";
+import userRoute from "./routes/user.routes.js";
+
 dotenv.config({});
 const app = express();
 
@@ -19,6 +21,8 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
      connectDB();
