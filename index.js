4d05ccf81cce7354b49711eb6daab220bd2087bb
VaibhavMatere <vpm00718897@gmail.com>
2024-10-09 00:13:53 +0530

    job routes added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import dotenv from 'dotenv'
 import { connectDB } from "./utils/db.js";
 import userRoute from "./routes/user.routes.js";
 import companyRoute from './routes/company.routes.js';
+import jobRoute from './routes/job.routes.js';
 
 dotenv.config({});
 const app = express();
@@ -25,6 +26,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
+app.use("/api/v1/job", jobRoute);
 
 app.listen(PORT, () => {
      connectDB();
