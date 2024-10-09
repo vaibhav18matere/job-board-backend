944076358e488d0e4cc2a490f100ac4509d0a280
VaibhavMatere <vpm00718897@gmail.com>
2024-10-09 17:04:46 +0530

    apply for a job controller added

---
+++ controllers/application.controller.js
@@ -0,0 +1,61 @@
+import { Application } from '../models/application.model.js';
+import { Job } from "../models/job.model.js";
+
+export const applyJob = async (req, res) => {
+     try {
+          const userId = req.id;
+          const jobId = req.params.id;
+
+          if (!jobId) {
+               return res.status(400).json({
+                    message: "Job ID is required!",
+                    success: false
+               });
+          };
+
+          // check if user is already applied for this job 
+
+          const existingApplication = await Application.findOne({
+               job: jobId,
+               applicant: userId
+          });
+
+          if (existingApplication) {
+               return res.status(400).json({
+                    message: "You have already applied for this job!",
+                    success: false,
+               });
+          };
+
+          // check if the job exists or not
+
+          const job = await Job.findById(jobId);
+
+          if (!job) {
+               return res.status(400).json({
+
+                    message: "Job not found!",
+                    success: false
+               });
+          };
+
+          // apply for new job if all above checks are okay
+
+          const newApplication = await Application.create({
+               job: jobId,
+               application: userId
+          });
+
+          job.applications.push(newApplication._id);
+
+          await job.save();
+
+          return res.status(201).json({
+               message: "Job applied successfully!",
+               success: true
+          });
+
+     } catch (error) {
+          console.log(error);
+     }
+};
