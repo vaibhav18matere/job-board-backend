947a7c982d179ec0e92934420b46c0895990992c
VaibhavMatere <vpm00718897@gmail.com>
2024-10-09 17:28:40 +0530

    get all applicants controller added

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -1,4 +1,4 @@
-import { Application } from '../models/application.model.js';
+import { Application } from "../models/application.model.js";
 import { Job } from "../models/job.model.js";
 
 export const applyJob = async (req, res) => {
@@ -9,15 +9,15 @@ export const applyJob = async (req, res) => {
           if (!jobId) {
                return res.status(400).json({
                     message: "Job ID is required!",
-                    success: false
+                    success: false,
                });
-          };
+          }
 
-          // check if user is already applied for this job 
+          // check if user is already applied for this job
 
           const existingApplication = await Application.findOne({
                job: jobId,
-               applicant: userId
+               applicant: userId,
           });
 
           if (existingApplication) {
@@ -25,7 +25,7 @@ export const applyJob = async (req, res) => {
                     message: "You have already applied for this job!",
                     success: false,
                });
-          };
+          }
 
           // check if the job exists or not
 
@@ -33,17 +33,16 @@ export const applyJob = async (req, res) => {
 
           if (!job) {
                return res.status(400).json({
-
                     message: "Job not found!",
-                    success: false
+                    success: false,
                });
-          };
+          }
 
           // apply for new job if all above checks are okay
 
           const newApplication = await Application.create({
                job: jobId,
-               application: userId
+               application: userId,
           });
 
           job.applications.push(newApplication._id);
@@ -52,40 +51,69 @@ export const applyJob = async (req, res) => {
 
           return res.status(201).json({
                message: "Job applied successfully!",
-               success: true
+               success: true,
           });
-
      } catch (error) {
           console.log(error);
      }
 };
 
-
 export const getAppliedJobs = async (req, res) => {
      try {
           const userId = req.id;
 
-          const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({
-               path: 'job',
-               options: { sort: { createdAt: -1 } },
-               populate: {
-                    path: 'company',
-                    option: { sort: { createdAt: -1 } },
-               }
-          });
+          const application = await Application.find({ applicant: userId })
+               .sort({ createdAt: -1 })
+               .populate({
+                    path: "job",
+                    options: { sort: { createdAt: -1 } },
+                    populate: {
+                         path: "company",
+                         option: { sort: { createdAt: -1 } },
+                    },
+               });
 
           if (!application) {
                return res.status(404).json({
                     message: "No application found!",
                     success: false,
                });
-          };
+          }
 
           return res.status(200).json({
                application,
-               success: true
+               success: true,
+          });
+     } catch (error) {
+          console.log(error);
+     }
+};
+
+
+// admin can check total applicants for specific job
+
+export const getApplicants = async (req, res) => {
+     try {
+          const jobId = req.params.id;
+          const job = await Job.findById(jobId).populate({
+               path: "applications",
+               options: { sort: { createdAt: -1 } },
+               populate: {
+                    path: "applicant",
+               },
           });
 
+          if (!job) {
+               return res.status(404).json({
+                    message: "Job(s) not found!",
+                    success: false,
+               });
+          }
+
+          return res.status(200).json({
+               job,
+               success: true,
+          });
      } catch (error) {
           console.log(error);
      }
