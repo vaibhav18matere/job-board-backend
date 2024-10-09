787e334b6743426515557f4673bc1fdb99684553
VaibhavMatere <vpm00718897@gmail.com>
2024-10-09 17:18:21 +0530

    get all applied jobs controller added

--- controllers/application.controller.js
+++ controllers/application.controller.js
@@ -59,3 +59,34 @@ export const applyJob = async (req, res) => {
           console.log(error);
      }
 };
+
+
+export const getAppliedJobs = async (req, res) => {
+     try {
+          const userId = req.id;
+
+          const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({
+               path: 'job',
+               options: { sort: { createdAt: -1 } },
+               populate: {
+                    path: 'company',
+                    option: { sort: { createdAt: -1 } },
+               }
+          });
+
+          if (!application) {
+               return res.status(404).json({
+                    message: "No application found!",
+                    success: false,
+               });
+          };
+
+          return res.status(200).json({
+               application,
+               success: true
+          });
+
+     } catch (error) {
+          console.log(error);
+     }
+};
